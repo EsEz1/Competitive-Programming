5281d7166ef4ed96b6c341587083de696296188d
EsEz1 <ezzateslam131@gmail.com>
2023-01-04 03:19:48 +0200

    Math Update

--- math/math.cpp
+++ math/math.cpp
@@ -1,11 +1,11 @@
 /*
-* The summation of Numbers from 1 to n
+* The summation of numbers from 1 to n
 
 * Suppose that you need to sum from 1 to 3
 
 * 1 + 2 + 3 = 6
 
-* you can use the following operation n*(n+1)/2 (n represents the number you need to reach it. From 1 to n).
+* You can use the following operation n*(n+1)/2 (n represents the number you need to reach it. From 1 to n).
 */
 int n;
 cin >> n;
@@ -35,7 +35,7 @@ cout << n * (n + 1) * (2n + 1) / 6;
 1 3 5 7 the difference between 1 and 3 is 2, 3 and 5 is 2, 5 and 7 is 2 so the difference is constant.
 
 * you can use the following operation n*(a+b)/2 (n represents the number of element so in this case, the number of elements is 4
-  (a represents the first element of the numbers (1)) and finally ( b reprsenets the last element of numbers (7)).
+  (a represents the first element of the numbers (1)) and finally (b reprsenets the last element of numbers (7)).
 */
 int a, b, c, d;
 cin >> a >> b >> c >> d;
